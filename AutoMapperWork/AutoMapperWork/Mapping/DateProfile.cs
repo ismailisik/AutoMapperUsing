24a329c1fb29aad339d4c9b41ec25b8ae9688fb4
ismailisik <i.isikl@outlook.com.tr>
2020-08-09 13:22:04 +0300

    Notlar Eklendi

--- AutoMapperWork/AutoMapperWork/Mapping/DateProfile.cs
+++ AutoMapperWork/AutoMapperWork/Mapping/DateProfile.cs
@@ -13,10 +13,16 @@ namespace AutoMapperWork.Mapping
     {
         public DateProfile()
         {
+            //Bu Kısımda AutoMapper kütüphanesinin projection işlemini inceledik. Bir prop'un verilerini birden çok prop'a mapledik.
+
+
+
             CreateMap<Date, DateDto>()
                 .ForMember(dest => dest.Year, opt => opt.MapFrom(x => x.date.Year))
                 .ForMember(dest => dest.Month, opt => opt.MapFrom(x => x.date.Month))
                 .ForMember(dest => dest.Day, opt => opt.MapFrom(x => x.date.Day));
+
+            //Not: Burada reverseMap methodu kullanılamamasından dolayı aşağıdaki gibi tersine mapleme yaptık.
 
             CreateMap<DateDto, Date>()
                 .ForMember(dest => dest.date, opt => opt.MapFrom(x => new DateTime(x.Year, x.Month, x.Day)));
